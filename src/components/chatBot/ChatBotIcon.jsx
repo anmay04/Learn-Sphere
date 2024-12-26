196c51680a427d5125b24c4bbe2e9eaeac9ed6f8
anmay04 <tripathianant199@gmail.com>
2024-12-26 18:40:51 +0530

    vercel deploy

--- src/components/chatBot/ChatBotIcon.jsx
+++ src/components/chatBot/ChatBotIcon.jsx
@@ -1,13 +1,14 @@
 // client/src/components/chatBot/ChatBotIcon.jsx
 import React from 'react';
-import { BsChatDotsFill } from 'react-icons/bs'; // Or your desired icon
+import { BsChatDotsFill } from 'react-icons/bs';
+import { GiAnimalSkull } from "react-icons/gi";
 const ChatBotIcon = ({ onClick }) => {
     return (
         <div
             className="fixed bottom-4 right-4 cursor-pointer bg-blue-500 text-white p-3 rounded-full shadow-lg hover:bg-blue-600 transition-all duration-200"
             onClick={onClick}
         >
-            <BsChatDotsFill size={25} />
+            <GiAnimalSkull size={25} />
         </div>
     );
 };
